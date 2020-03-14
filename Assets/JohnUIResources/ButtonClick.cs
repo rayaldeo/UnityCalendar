c0b1d3472a0de2f4941b6f74ddf0fcfeb02ce24c
Jonathan Elliott <jn288377@dal.ca>
2020-03-14 14:08:12 -0400

    Adding in John's UI

---
+++ Assets/JohnUIResources/ButtonClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonClick : MonoBehaviour
+{
+
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log("Button clicked");
+    }
+}
